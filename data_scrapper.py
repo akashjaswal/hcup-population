66fbc0b7b228523f3600dcb37c4abdf98cd6b4d1
Akashdeep Jaswal <akashdeepsinghjaswal@gmail.com>
2016-01-15 16:51:25 -0800

    Optimized function calls and removed redundancies

--- data_scrapper.py
+++ data_scrapper.py
@@ -4,6 +4,7 @@
 import requests 
 from BeautifulSoup import BeautifulSoup
 import csv
+import os
 
 homepage = "http://www.census.gov" # United States Census Bureau
 
@@ -17,17 +18,10 @@ def fileTemplate(htmlOutput):
 	template = str(htmlOutput.find('a',attrs={'title': 'File layout'}))[9:73]
 	template = homepage + template
 	data = getHTML(template)
-	#print data
-	header = [['Year','FIPS_state_code','FIPS_county_code','Age_group','Race-Sex','Ethnic_origin','population']]
-	csvWriter('data_1990_1999.csv', 'w', header)
-	print "Template creation successful"
-
-def csvWriter(fileName, mode, data):
-	open_csv = open(fileName, mode, newline='')
-	a = csv.writer(open_csv)
-	a.writerows(data)
-	open_csv.close()
-
+	print data
+	header = ['Year','FIPS_state_code','FIPS_county_code','Age_group','Race-Sex','Ethnic_origin','population']
+	dataWriter(data,"data_1990_1999_template.txt")
+	
 # Extracting each individual URLs for txt files containing data. 
 def extractData(htmlOutput):
 	table = htmlOutput.find('table')
@@ -38,17 +32,20 @@ def extractData(htmlOutput):
 	for url in rows:
 		url = homepage + url[9:81]
 	 	data = getHTML(url)
-	 	csvWriter('data_1990_1999.csv', 'a', data)
-	 	print "data write successful"
+	 	dataWriter(data,"raw_data_1990_1999.txt")
 
-def getContent(url):
-	response = requests.get(url)
-	data = BeautifulSoup(response.content)
+def dataWriter(data,filename):
+	txt_file = open(filename, "a")
+	txt_file.write(str(data))
+	txt_file.close()
+	print "Data written to file %s successfully" % filename 
 
 # Parsing the webpage with Intercensal data 1990-1999	
 output = getHTML("http://www.census.gov/popest/data/intercensal/st-co/characteristics.html")
 template = fileTemplate(output)
 data = extractData(output)
+
+
 
 
 
